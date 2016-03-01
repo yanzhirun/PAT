af9c84b51598abeec465a768e64b025ac4222ceb
yanzhirun <runmissing@gmail.com>
2016-03-01 12:44:47 +0800

    bata 1.0

--- practice/store.c
+++ practice/store.c
@@ -4,12 +4,26 @@
 
 int main(int argc, char * argv[])
 {
-	printf("bata 0.2\n");
+	int num;
+	double price;
+	double discount;
 
-//1.输入商品数量
-//2.输入价格
-//3.打折额度
-//4.计算对应款项
+	printf("bata 1.0\n");
+
+	//1.输入商品数量
+	printf("please input the number of things:");
+	scanf("%d",&num);
+
+	//2.输入价格
+	printf("please input the price of things:");
+	scanf("%lf",&price);
+	
+	//3.打折额度
+	printf("please input the discount:");
+	scanf("%lf",&discount);
+
+	//4.计算对应款项
+	printf("the totle price: %.2f $\n", num * price * discount);
 
 	return 0;
 }
