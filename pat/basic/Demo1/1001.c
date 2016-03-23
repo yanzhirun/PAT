3d5ec5d05f2d0cf7185473c74226dc38ce330f79
yanzhirun <runmissing@gmail.com>
2016-03-23 11:14:10 +0800

    add 1001 & 1002 correct

---
+++ pat/basic/Demo1/1001.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int callatz(int N, int i)
+{
+	int err = 0;
+	int n = N;
+while(n != 1)
+{
+	if (n%2 == 0)
+	{
+		n = n/2;
+		i++;
+		return callatz(n, i);
+	}
+	else
+	{
+		n = (n * 3 + 1)/2;
+		i++;
+		return callatz(n, i);
+	}
+//	return err;
+}
+printf("%d\n", i);
+}
+void main()
+{
+	int N;
+	int ret;
+	//printf("Please input the number:");
+	scanf("%d",&N);
+	ret = callatz(N, 0);
+
+	return ;
+}
+
---
+++ pat/basic/Demo1/1002.c
@@ -0,0 +1,64 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
+#include "math.h"
+
+int print_num(char num[101])
+{
+	int err = 0;
+	int i;
+	int result = 0;
+	char *pinyin[10] = {"ling", "yi", "er", "san", "si", "wu", "liu", "qi", "ba", "jiu"};
+	if (num == NULL)
+	{
+		err = 1;
+		printf("wrong num :%d \n", err);
+		return err;
+	}
+
+	for (i = 0; i <= 100; i++)
+	{
+		if(num[i] != 0)
+		{
+			result += (int)(num[i] - '0');
+//			printf("numi%d",(int)(num[i] ));
+		}
+	}
+//	printf("result%d\n", result);
+
+//打印piny
+	int j , s, a[3]={0},k;
+	s = result;
+	for ( j = 0; j < 3; j++)
+	{
+		a[j] = s%10;
+//		printf("---==");
+//		printf("a[%d]=%d \n", j, a[j]);
+		s /= 10;
+	}
+//	printf("\npinyin====\n");
+	if (a[2] != 0)
+	{
+		printf("%s %s %s",pinyin[a[2]],pinyin[a[1]],pinyin[a[0]]);
+	}
+	else if (a[1] != 0)
+	{
+		printf("%s %s",pinyin[a[1]], pinyin[a[0]]);
+	}
+	else
+	{
+		printf("%s",pinyin[a[0]]);
+	}
+
+	return err;
+}
+
+int main()
+{
+	char num[101] = {'0'};
+	
+	scanf("%100s", num);
+	print_num(num);
+
+	return 0;
+}
