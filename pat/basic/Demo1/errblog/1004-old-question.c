776eb6e07f0b7c59775db051fc34a1b09e4c8023
yanzhirun <runmissing@gmail.com>
2016-03-28 13:18:10 +0800

    submit 1004 OK! creat errblog! scanf struct has question!

--- pat/basic/Demo1/errblog/1004-old-question.c
+++ pat/basic/Demo1/errblog/1004-old-question.c
@@ -7,7 +7,6 @@ struct _Student
     char name[16];
     char stunum[16];
     int score;
-
 };
 
 struct _Student* creat_stu(int num)
@@ -22,7 +21,6 @@ struct _Student* creat_stu(int num)
     {
         memset(student+i, 0, sizeof(struct _Student));
     }
-
     return student;
 }
 
@@ -33,16 +31,50 @@ void free_struct_stu(int num, struct _Student* student)
     {
         return;
     }
+    /*
+    for (i = 0; i < num; i++)
+    {
+        char * pname = student[i].name;
+        if (NULL != pname)
+        {
+            free(pname);
+        }
+        char * pstunum = student[i].stunum;
+        if(NULL != pstunum)
+        {
+            free(pstunum);
+        }
+        if (NULL != student)
+        {
+            free(student);
+        }
+    }
+*/
     if (NULL != student)
     {
         free(student);
     }
 }
 
+struct _Student* scanf_stu(int num, struct _Student* student)
+{
+    int i;
+    for (i = 0; i < num; i++)
+    {
+         scanf("%s", student[i].name);
+         scanf("%s", student[i].stunum);
+         scanf("%d", &(student[i].score));
+    }
+/*    for (i = 0; i < num; i++)
+    {
+        printf("name:%s  number:%s  score:%d\n", student[i].name, student[i].stunum, student[i].score);
+    }
+*/
+}
+
 void findstu(int n, struct _Student* stu)
 {
     int i, min_stu = 0, max_stu = 0;
-
     for (i = 0; i < n; i++)
     {
         if (stu[min_stu].score > stu[i].score)
@@ -56,25 +88,18 @@ void findstu(int n, struct _Student* stu)
 
 int main()
 {
-    int n;
-    scanf("%d", &n);
-    struct _Student * stu = creat_stu(n);
-    //scanf_stu(n, stu);
+    int i, n;
+    struct _Student *stu;
     if (NULL == stu)
     {
         printf("stu created err\n");
         return 0;
     }
-    int i;
-    for (i = 0; i < n; i++)
-    {
-        scanf("%s", stu[i].name);
-        scanf("%s", stu[i].stunum);
-        scanf("%d", &(stu[i].score));
-    }
+    scanf("%d", &n);
+    stu = creat_stu(n);
+    scanf_stu(n, stu);
     findstu(n, stu);
     free_struct_stu(n, stu);
-    //system("pause");
 
     return 0;
 }
