b74d0f13dc8dd9e0c599a24fbd98087fb098b816
yanzhirun <runmissing@gmail.com>
2016-04-01 00:39:08 +0800

    1008 pass! ok!

--- pat/basic/Demo1/1008.c
+++ pat/basic/Demo1/1008.c
@@ -1,6 +1,5 @@
 #include "stdio.h"
 #include "stdlib.h"
-#include "string.h"
 
 int * creat_arr(int n)
 {
@@ -15,7 +14,7 @@ int * creat_arr(int n)
 
 int main()
 {
-    int n, m, i;
+    int n, m, i, p;
     int *arr = NULL;
     scanf("%d %d", &n, &m);
     //printf("%d  %d", n, m);
@@ -28,19 +27,29 @@ int main()
     //printf("size:%d", sizeof(arr));
     //printf("before m:%d",m);
     //m=n-m%n;
-    if (m > n)
+    if (n < 1 || n > 100)
+    {
+         return 0;
+    }
+    else if (m >= n)
     {
         int i = 1;
         do
         {
-            m = m - n*i;
+            p = m - n*i;
             i++;
-        }while(m > n);
+        }while(p >= n );
+        m = p;
     }
-    //printf("after m%d",m);
+    //printf("after m%d",p);
     for (i = 0; i < n; i++)
     {
         scanf("%d",arr+i);
+    }
+    if (n == 1)
+    {
+        printf("%d", arr[0]);
+        return 0;
     }
     for (i = n-m; i < n; i++)
     {
@@ -51,6 +60,11 @@ int main()
         printf("%d ",arr[i]);
     }
     printf("%d",arr[n-m-1]);
+    if (NULL != arr)
+    {
+        free(arr);
+        arr = NULL;
+    }
 
     return 0;
 }
