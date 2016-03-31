31203bfa45f1cdb119d4a1e38fa08d708352b5b7
yanzhirun <runmissing@gmail.com>
2016-03-31 21:15:45 +0800

    1007 submited OK!

--- pat/basic/Demo1/1007.c
+++ pat/basic/Demo1/1007.c
@@ -1,12 +1,13 @@
 #include "stdlib.h"
 #include "stdio.h"
-#include "string.h"
+#include "math.h"
 
 int primetest(int input)
 {
     int err = 0;
     int i;
-    for (i = 2; i < input; i++)
+
+    for (i = 2; i <= sqrt(input); i++)
     {
         if ( input%i == 0 )
         {
@@ -21,17 +22,17 @@ int primetest(int input)
 int main()
 {
     int i, j = 0, n = 0, k = 0, num;
-    int prime[1000] = {0};
+    int prime[10000] = {0};
     scanf("%d", &num);
 
-    printf("num:%d is %d(0 yes ,1 no) \n",num , primetest(num));
+//    printf("num:%d is %d(0 yes ,1 no) \n",num , primetest(num));
 
-    for ( i = 3; i < num; i++ )
+    for ( i = 2; i <= num; i++ )
     {
          if (primetest(i) == 0)
          {
             prime[j] = i;
-            printf("prime%d\n", prime[j]);
+            //printf("prime%d\n", prime[j]);
             j++;
          }
          else{
@@ -44,8 +45,9 @@ int main()
          if(prime[k] - prime[k-1] == 2)
          {
            n++;
+          // printf("prime[%d]:%d",k,prime[k]);
          }
-    }while(prime[k] = 0);
+    }while(prime[k] != 0);
     printf("%d", n);
     return 0;
 }
