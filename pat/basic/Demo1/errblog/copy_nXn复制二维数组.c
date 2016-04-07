ccd46e8515be098a5cd5fb71b14a698863074056
yanzhirun <runmissing@gmail.com>
2016-04-07 16:55:09 +0800

    add copy_n X m 数组

---
+++ pat/basic/Demo1/errblog/copy_nXn复制二维数组.c
@@ -0,0 +1,96 @@
+#include "stdlib.h"
+#include "stdio.h"
+
+int ** inputABC(int line_num, int row_num)
+{
+    int i;
+    int **input = NULL;
+    input = (int **)malloc(line_num*sizeof(int*));
+    if (NULL == input)
+    {
+        printf("malloc err");
+        return NULL;
+    }
+    for (i = 0; i < line_num; i++)
+    {
+        input[i] = (int *)malloc(row_num*sizeof(int));
+    }
+
+    return input;
+}
+
+int input_free(int ** input,int line_num)
+{
+    int i;
+
+    if (NULL == input)
+    {
+         return 0;
+    }
+    for (i = 0; i < line_num; i++)
+    {
+        if (NULL != input[i])
+        {
+            free(input[i]);
+            input[i] = NULL;
+        }
+    }
+    if (NULL != input)
+        free(input);
+
+    return 0;
+}
+
+int main()
+{
+    int line_num=3, row_num=3, i, j;
+    int **input = NULL;
+
+    input = inputABC(line_num, 3);
+
+    for (i = 0; i < line_num; i++)
+    {
+        for (j = 0; j < row_num; j++)
+        {
+            scanf("%d", &input[i][j]);
+        }
+    }
+    printf("=====diyige:\n");
+    for (i = 0; i < line_num; i++)
+    {
+        for (j = 0; j < row_num; j++)
+        {
+            printf("%d ", input[i][j]);
+        }
+        printf("\n");
+    }
+    printf("=====\n");
+
+//复制 n*3 二维数组
+   // int (*p_copy)[3];
+    int coparr[3][3];
+    for (i = 0; i < 3; i++)
+    {
+        //*coparr[i] = *input[i];
+        memcpy(*(coparr+i), *(input+i), sizeof(int)*3);
+    }
+
+    if (NULL != input)
+    {
+        input_free(input, row_num);
+        input = NULL;
+    }
+
+    printf("\ncopp\n=====\n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("%d ", coparr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\ncopp\n=====\n");
+//        printf("input[i]:%d\n", input[0]);
+    return 0;
+}
