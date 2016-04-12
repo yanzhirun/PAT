e3b1d2daf736a8fa185023c181c1f5e9b5806064
yanzhirun <runmissing@gmail.com>
2016-04-12 10:28:19 +0800

    1020 bata2 数组按列输入+qsort排序

--- pat/basic/Demo1/1020.c
+++ pat/basic/Demo1/1020.c
@@ -1,38 +1,65 @@
 #include "stdlib.h"
 #include "stdio.h"
 
+float **create_arr (int N)
+{
+    int i;
+    float ** p_f_array = NULL;
+    p_f_array = (float **)malloc(sizeof(float*)*N);
+    for (i = 0; i < N; i++)
+    {
+        p_f_array[i] = (float*)malloc(sizeof(float)*3);
+    }
+
+    return p_f_array;
+}
+
+int free_array (float ** array, int N)
+{
+    int i;
+
+    if (NULL == array)
+    {
+        printf ("free_array  err\n");
+        return 0;
+    }
+    for (i = 0; i < N; i++)
+    {
+         free (array[i]);
+         array[i] = NULL;
+    }
+    free (array);
+
+    return 0;
+}
+
 int cmp (const void *a, const void *b)
 {
-    return (float*)b - (float*)a;
+    float **left = (float **)a;
+    float **right = (float **)b;
+
+    return (*left)[2] < (*right)[2];
 }
-int calculate (int i_kind, int i_need, int * p_store, int * p_total_price)
+
+int calculate (int i_kind, int i_need, float **p_commodity)
 {
     int ret = 1, i = 0;
-    float *p_price_single = NULL;
-    if (NULL == p_store || NULL == p_total_price)
+    if (NULL == p_commodity)
     {
         ret = 0;
         printf ("calculate err\n");
         return ret;
     }
 
-    p_price_single = (float*)malloc(sizeof(float)*i_kind);
+    for (i = 0; i < i_kind; i++)
+    {
+        p_commodity[i][2] = p_commodity[i][1]/p_commodity[i][0];
+    }
+    qsort (p_commodity, i_kind, sizeof(float), cmp);
 
     for (i = 0; i < i_kind; i++)
     {
-        p_price_single[i] = (float)p_total_price[i]/(float)p_store[i];
-    }
-    printf ("%d",p_total_price[0]);
-//    qsort (p_total_price, i_kind, sizeof(float), cmp);
-
-    for (i = 0; i < i_kind; i++)
-    {
-        printf ("danjia:%f ", p_price_single[i]);
-    }
-    if (NULL != p_price_single)
-    {
-        free (p_price_single);
-        p_price_single = NULL;
+        printf ("danjia :%.2f   \n", p_commodity[i][2]);
     }
 
     return ret;
@@ -43,37 +70,30 @@ int main()
     int i_kind, i_need, i;
     int *p_store = NULL;
     int *p_total_price = NULL;
+    float **p_commodity = NULL;
 
     scanf ("%d%d", &i_kind, &i_need);
 
-    p_store = (int*)malloc(sizeof(int)*i_kind);
-    p_total_price = (int*)malloc(sizeof(int)*i_kind);
+    p_commodity = create_arr(i_kind);
     for (i = 0; i < i_kind; i++)
     {
-        scanf ("%d", p_store+i);
+    //    scanf ("%f", &p_commodity[i][0]);
+    //    scanf ("%f", (p_commodity+i)[0]);
+        scanf ("%f", *(p_commodity+i));
     }
     for (i = 0; i < i_kind; i++)
     {
-        scanf ("%d", p_total_price+i);
+        //scanf ("%f", (p_commodity+i)[1]);//wrong!!!按列输入！取地址，数组内容在内存中连续存储
+        scanf ("%f", *(p_commodity+i)+1);
     }
 
-    calculate(i_kind, i_need, p_store, p_total_price);
+    calculate(i_kind, i_need, p_commodity);
 
-//    for (i = 0; i < i_kind; i++)
-//    {
-//        printf ("%d ", p_store[i]);
-//        printf ("%d ", p_total_price[i]);
-//    }
-
-    if (NULL != p_store)
+    for (i = 0; i < i_kind; i++)
     {
-         free (p_store);
-         p_store = NULL;
+        printf("%f   %f    ", p_commodity[i][0], p_commodity[i][1]);
+        printf ("danjia :%.2f   \n", p_commodity[i][2]);
     }
-    if (NULL != p_total_price)
-    {
-         free (p_total_price);
-         p_total_price = NULL;
-    }
+
     return 0;
 }
