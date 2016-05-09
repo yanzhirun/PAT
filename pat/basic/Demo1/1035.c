4965b1f85cbb7d3dc3059711764cd00420137601
yanzhirun <runmissing@gmail.com>
2016-05-09 09:40:43 +0800

    1035 bata 1

--- pat/basic/Demo1/1035.c
+++ pat/basic/Demo1/1035.c
@@ -1,23 +1,75 @@
 #include "stdlib.h"
 #include "stdio.h"
 
+int min (int x, int y)
+{
+    return x < y ? x : y;
+}
 int merge_sort_test (int* input, int* sort_first, int N)
 {
-
     int ret = 0, i, j, k, tmp, cmp_same = 0;
-    int insert[101];
+    int* a = input;
+    int* b = (int*)malloc(N * sizeof(int*));
+    int seg, start, low, mid, high, start1, end1, start2, end2;
     if (NULL == input || NULL == sort_first)
     {
         ret = 1;
         return ret;
     }
-    for (i = 0; i < N; i++)
+
+    for (seg = 1; seg < N; seg += seg)
     {
-        insert[i] = input[i];
+         for (start = 0; start < N; start += seg + seg)
+         {
+             low = start;
+             mid = min (start + seg, N);
+             high = min (start + seg + seg, N);
+             k = low;
+             start1 = low;
+             end1 = mid;
+             start2 = mid;
+             end2 = high;
+             while (start1 < end1 && start2 < end2)
+                 b[k++] = a[start1] < a[start2] ? a[start1++] : a[start2++];
+
+             while (start1 < end1)
+                 b[k++] = a[start1++];
+             while(start2 < end2)
+                 b[k++] = a[start2++];
+         }
+         int*temp = a;
+         a = b;
+         b = temp;
+        if (cmp_same)
+         {
+             printf("Merge Sort\n");
+             for (i = 0; i < N-1; i++)
+             {
+                 printf("%d ", b[i]);
+             }
+                 printf("%d", b[N-1]);
+         }
+         for (k = 0; k < N; k++)
+         {
+             cmp_same = 1;
+             if (b[k] != sort_first[k])
+             {
+                 cmp_same = 0;
+                 break;
+             }
+         }
     }
-
-
-
+//    if (a != input)
+//    {
+//        for (i = 0; i < N; i++)
+//            b[i] = a[i];
+//        b = a;
+//    }
+    if (NULL != b)
+    {
+        free(b);
+        b = NULL;
+    }
 
     return ret;
 }
@@ -44,11 +96,12 @@ int insert_sort_test (int* input, int* sort_first, int N)
         insert[j+1]= tmp;
         if (cmp_same)
         {
-            printf("insert sort\n");
-            for (i = 0; i < N; i++)
+            printf("Insertion Sort\n");
+            for (i = 0; i < N-1; i++)
             {
                 printf("%d ", insert[i]);
             }
+                printf("%d", insert[N-1]);
         }
         for (k = 0; k < N; k++)
         {
@@ -77,7 +130,6 @@ int main()
         scanf("%d", sort_first+i);
     }
     insert_sort_test (input, sort_first, N);
-
     merge_sort_test (input, sort_first, N);
     return 0;
 }
