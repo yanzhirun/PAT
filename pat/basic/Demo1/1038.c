315078e0f4fb926705213e1e593862755fcc5db8
yanzhirun <runmissing@gmail.com>
2016-05-12 12:03:56 +0800

    1038 OK

--- pat/basic/Demo1/1038.c
+++ pat/basic/Demo1/1038.c
@@ -5,38 +5,23 @@ int main()
 {
     int N, K, i, j, k, tmp = 0;
     int score[100000], find[100000];
+    int result[101] = {0};
     scanf ("%d", &N);
     for (i = 0; i < N; i++)
     {
         scanf ("%d", score+i);
+        result[score[i]]++;
     }
     scanf ("%d", &K);
     for (i = 0; i < K; i++)
     {
         scanf ("%d", find+i);
     }
-    for (i = 0; i < K; i++)
+    for (i = 0; i < K - 1; i++)
     {
-        tmp = 0;
-        for (j = 0; j < N; j++)
-        {
-            if (find[i] == score[j])
-                tmp++;
-        }
-        printf("%d", tmp);
-        if (i < K-1)
-            printf(" ");
+        printf("%d ",result[find[i]]);
     }
-//    printf("\noutput----\n");
-//    for (i = 0; i < N; i++)
-//    {
-//        printf("%d  ", score[i]);
-//    }
-//    printf("\n----\n");
-//    for (i = 0; i < K; i++)
-//    {
-//        printf("%d  ", find[i]);
-//    }
+        printf("%d",result[find[K-1]]);
 
     return 0;
 }
